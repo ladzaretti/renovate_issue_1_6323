cb2b0ba3de719ca17e08946cdc2015c14772da36
Nitrael86 <martin.osanger@gmx.at>
2020-05-26 16:16:49 +0200

    update sample

---
+++ src/main/java/org/gradle/api/PersonList.java
@@ -0,0 +1,11 @@
+package org.gradle.api;
+
+import java.util.Date;
+
+public class PersonList {
+    private Date date = null;
+
+    public void doSomething() {
+        date = new Date();
+    }
+}
